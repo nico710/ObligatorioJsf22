6d1c1428f28521ca31db5186193f37c5425bc889
Nicolas <nicope710@gmail.com>
2014-10-10 16:07:15 -0200

    add

---
+++ src/main/java/com/npe/jsf/dataType/User.java
@@ -0,0 +1,203 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.npe.jsf.dataType;
+
+/**
+ *
+ * @author NicolasP1
+ */
+public class User {
+    
+    private String rol;
+    private String name;
+    private String surname;
+    private String CI;
+    private int birthday;
+    private String mail;
+    private String country;
+    private String pathPhoto;
+    
+    private String fatherName;
+    private String motherName;
+    private String fatherSurname;
+    private String motherSurname;
+    private int fatherBirthday;
+    private int motherBirthday;
+    
+    private String companyName;
+    private String addresCompany;
+    private int dateEntryCompany;
+    
+    private String previousCompanyName;
+    private String previousAddresCompany;
+    private int previusDateEntry;
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCI() {
+        return CI;
+    }
+
+    public void setCI(String CI) {
+        this.CI = CI;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPathPhoto() {
+        return pathPhoto;
+    }
+
+    public void setPathPhoto(String pathPhoto) {
+        this.pathPhoto = pathPhoto;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public String getFatherSurname() {
+        return fatherSurname;
+    }
+
+    public void setFatherSurname(String fatherSurname) {
+        this.fatherSurname = fatherSurname;
+    }
+
+    public String getMotherSurname() {
+        return motherSurname;
+    }
+
+    public void setMotherSurname(String motherSurname) {
+        this.motherSurname = motherSurname;
+    }
+
+    public int getFatherBirthday() {
+        return fatherBirthday;
+    }
+
+    public void setFatherBirthday(int fatherBirthday) {
+        this.fatherBirthday = fatherBirthday;
+    }
+
+    public int getMotherBirthday() {
+        return motherBirthday;
+    }
+
+    public void setMotherBirthday(int motherBirthday) {
+        this.motherBirthday = motherBirthday;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAddresCompany() {
+        return addresCompany;
+    }
+
+    public void setAddresCompany(String addresCompany) {
+        this.addresCompany = addresCompany;
+    }
+
+    public int getDateEntryCompany() {
+        return dateEntryCompany;
+    }
+
+    public void setDateEntryCompany(int dateEntryCompany) {
+        this.dateEntryCompany = dateEntryCompany;
+    }
+
+    public String getPreviousCompanyName() {
+        return previousCompanyName;
+    }
+
+    public void setPreviousCompanyName(String previousCompanyName) {
+        this.previousCompanyName = previousCompanyName;
+    }
+
+    public String getPreviousAddresCompany() {
+        return previousAddresCompany;
+    }
+
+    public void setPreviousAddresCompany(String previousAddresCompany) {
+        this.previousAddresCompany = previousAddresCompany;
+    }
+
+    public int getPreviusDateEntry() {
+        return previusDateEntry;
+    }
+
+    public void setPreviusDateEntry(int previusDateEntry) {
+        this.previusDateEntry = previusDateEntry;
+    }
+    
+    
+    
+            
+           
+    
+}
