c8441a8c29387b8ded0ce9dadf0834450f606aa6
Nicolas <nicope710@gmail.com>
2014-10-21 11:40:55 -0200

    add

---
+++ src/main/java/com/npe/jsf/functions/CustomFunctions.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.npe.jsf.functions;
+
+/**
+ *
+ * @author NicolasP1
+ */
+public class CustomFunctions {
+    
+        private CustomFunctions() {
+        // Hide constructor.
+    }
+
+    public static boolean isAdmin(String rol) {
+        System.out.println("User Rol: " + rol);
+        return "ADMIN".equalsIgnoreCase(rol.trim());
+    }
+    
+}
